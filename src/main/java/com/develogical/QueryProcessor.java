1de7ef9c87209bcacc7c0304578887e511963be2
Radovan1997 <rvitek41@gmail.com>
2021-02-02 09:59:58 +0100

    trying to break the build

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -2,6 +2,8 @@ package com.develogical;
 
 public class QueryProcessor {
 
+    ewngoiwnafgwoingioqangoě
+
     public String process(String query) {
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
